349ac4cec2acb014f316b197d2055bd00771459a
James Galantino <james_galantino@aol.com>
2021-06-08 11:38:37 -0400

    Created and imported menu bar

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import "semantic-ui-css/semantic.min.css";
 import "./App.css";
 
+import MenuBar from "./components/MenuBar";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -11,6 +12,7 @@ import Register from "./pages/Register";
 function App() {
   return (
     <Router>
+      <MenuBar />
       <Route exact path="/" component={Home} />
       <Route exact path="/login" component={Login} />
       <Route exact path="/register" component={Register} />
