1a6bf8257faf2a09752604b3cc1526a0e12aba5b
James Galantino <james_galantino@aol.com>
2021-06-10 11:18:21 -0400

    Added comment button, placeholder functionality

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -10,6 +10,10 @@ function PostCard({
     console.log("Like post!");
   }
 
+  function commentOnPost() {
+    console.log("Comment on post!");
+  }
+
   return (
     <Card fluid>
       <Card.Content>
@@ -31,6 +35,14 @@ function PostCard({
           </Button>
           <Label basic color="teal" pointing="left">
             {likeCount}
+          </Label>
+        </Button>
+        <Button as="div" labelPosition="right" onClick={commentOnPost}>
+          <Button color="blue" basic>
+            <Icon name="comments" />
+          </Button>
+          <Label basic color="blue" pointing="left">
+            {commentCount}
           </Label>
         </Button>
       </Card.Content>
