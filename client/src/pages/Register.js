9ab72a93d13d1430ba1ab008769411e7bfeda7b5
James Galantino <james_galantino@aol.com>
2021-06-10 12:09:52 -0400

    Added form field types

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -35,6 +35,7 @@ function Register() {
           label="Username"
           placeholder="Username"
           name="username"
+          type="text"
           value={values.username}
           onChange={onChange}
         />
@@ -42,6 +43,7 @@ function Register() {
           label="Email"
           placeholder="Email"
           name="email"
+          type="email"
           value={values.email}
           onChange={onChange}
         />
@@ -49,6 +51,7 @@ function Register() {
           label="Password"
           placeholder="Password"
           name="password"
+          type="password"
           value={values.password}
           onChange={onChange}
         />
@@ -56,6 +59,7 @@ function Register() {
           label="Confirm Password"
           placeholder="Confirm Password"
           name="confirmPassword"
+          type="password"
           value={values.confirmPassword}
           onChange={onChange}
         />
