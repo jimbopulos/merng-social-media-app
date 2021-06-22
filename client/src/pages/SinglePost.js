46eb4b2d5198134d692887b9d3073b6a14dd265f
James Galantino <james_galantino@aol.com>
2021-06-22 18:42:22 -0400

    Added fetch posts query mutation, use query hook

--- client/src/pages/SinglePost.js
+++ client/src/pages/SinglePost.js
@@ -2,7 +2,41 @@ import React from "react";
 import gql from "graphql-tag";
 import { useQuery } from "@apollo/client";
 import { Grid } from "semantic-ui-react";
+import { FETCH_POSTS_QUERY } from "../utils/graphql";
 
-function SinglePost(props) {}
+function SinglePost(props) {
+  const postId = props.match.params.postId;
+  console.log(postId);
+
+  const {
+    data: { getPost },
+  } = useQuery(FETCH_POST_QUERY, {
+    variables: {
+      postId,
+    },
+  });
+}
+
+const FETCH_POST_QUERY = gql`
+  query($postId: ID!) {
+    getPost(postId: $postId) {
+      id
+      body
+      createdAt
+      username
+      likeCount
+      likes {
+        username
+      }
+      commentCount
+      comments {
+        id
+        username
+        createdAt
+        body
+      }
+    }
+  }
+`;
 
 export default SinglePost;
