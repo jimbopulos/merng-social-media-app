51024b53ceb2bb50e6b7a205e5b4fb259f6f283e
James Galantino <james_galantino@aol.com>
2021-06-22 18:53:00 -0400

    Added conditional render for single post component

--- client/src/pages/SinglePost.js
+++ client/src/pages/SinglePost.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import gql from "graphql-tag";
 import { useQuery } from "@apollo/client";
-import { Grid } from "semantic-ui-react";
-import { FETCH_POSTS_QUERY } from "../utils/graphql";
+import { Image, Button, Icon, Label, Card, Grid } from "semantic-ui-react";
+import moment from "moment";
+
+import { AuthContext } from "../context/auth";
+import LikeButton from "../components/LikeButton";
 
 function SinglePost(props) {
   const postId = props.match.params.postId;
+  const { user } = useContext(AuthContext);
   console.log(postId);
 
   const {
@@ -15,6 +19,61 @@ function SinglePost(props) {
       postId,
     },
   });
+
+  let postMarkup;
+  if (!getPost) {
+    postMarkup = <p>Loading post...</p>;
+  } else {
+    const {
+      id,
+      body,
+      createdAt,
+      username,
+      comments,
+      likes,
+      likeCount,
+      commentCount,
+    } = getPost;
+
+    postMarkup = (
+      <Grid>
+        <Grid.Row>
+          <Grid.Column width={2}>
+            <Image
+              src="https://react.semantic-ui.com/images/avatar/large/molly.png"
+              size="small"
+              float="right"
+            />
+          </Grid.Column>
+          <Grid.Column width={2}>
+            <Card fluid>
+              <Card.Content>
+                <Card.Header>{username}</Card.Header>
+                <Card.Meta>{moment(createdAt).fromNow()}</Card.Meta>
+                <Card.Description>{body}</Card.Description>
+              </Card.Content>
+              <hr />
+              <Card.Content extra>
+                <LikeButton user={user} post={{ id, likeCount, likes }} />
+                <Button
+                  as="div"
+                  labelPosition="right"
+                  onClick={() => console.log("Comment on post")}
+                >
+                  <Button basic color="blue">
+                    <Icon name="ocmments" />
+                    <Label basic color="blue" pointing="left">
+                      {commentCount}
+                    </Label>
+                  </Button>
+                </Button>
+              </Card.Content>
+            </Card>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    );
+  }
 }
 
 const FETCH_POST_QUERY = gql`
