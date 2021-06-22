b4873f9811825b5ffe435bc7c16cfd4afe10dab1
James Galantino <james_galantino@aol.com>
2021-06-22 19:06:52 -0400

    Added jsx and mutation to delete button component, conditiona render for confirm window

--- client/src/components/DeleteButton.js
+++ client/src/components/DeleteButton.js
@@ -1,20 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import gql from "graphql-tag";
 import { useMutation } from "@apollo/client";
 
-import { Button, Icon } from "semantic-ui-react";
+import { Button, Confirm, Icon } from "semantic-ui-react";
 
-function DeleteButton(props) {
+function DeleteButton({ postId }) {
+  const [confirmOpen, setConfirmOpen] = useState(false);
+
+  const [deletePost] = useMutation(DELETE_POST_MUTATION, {
+    update() {
+      setConfirmOpen(false);
+      // TODO: remove post from cache
+    },
+    variables: {
+      postId,
+    },
+  });
+
   return (
-    <Button
-      as="div"
-      color="red"
-      floated="right"
-      onClick={() => console.log("Delete post")}
-    >
-      <Icon name="trash" style={{ margin: 0 }} />
-    </Button>
+    <>
+      <Button
+        as="div"
+        color="red"
+        floated="right"
+        onClick={() => console.log("Delete post")}
+      >
+        <Icon name="trash" style={{ margin: 0 }} />
+      </Button>
+      <Confirm
+        open={confirmOpen}
+        onCancel={() => setConfirmOpen(false)}
+        onConfirm={deletePost}
+      />
+    </>
   );
 }
+
+const DELETE_POST_MUTATION = gql`
+  mutation deletePost($postId: ID!) {
+    deletePost($postId: postId)
+  }
+`;
 
 export default DeleteButton;
