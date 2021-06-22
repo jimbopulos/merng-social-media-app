cd53360fb21160e9ca36ec6f2b1d13d62800b368
James Galantino <james_galantino@aol.com>
2021-06-22 18:37:56 -0400

    Created route for single post page, as well as base component

---
+++ client/src/pages/SinglePost.js
@@ -0,0 +1,8 @@
+import React from "react";
+import gql from "graphql-tag";
+import { useQuery } from "@apollo/client";
+import { Grid } from "semantic-ui-react";
+
+function SinglePost(props) {}
+
+export default SinglePost;
