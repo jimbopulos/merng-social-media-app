2fcab167d8fae193f70c86f2373d57e13c83575f
James Galantino <james_galantino@aol.com>
2021-06-23 14:11:49 -0400

    Modified delete btn mutation to work dynamically for either posts or comments

--- client/src/components/DeleteButton.js
+++ client/src/components/DeleteButton.js
@@ -5,28 +5,33 @@ import { useMutation } from "@apollo/client";
 import { Button, Confirm, Icon } from "semantic-ui-react";
 import { FETCH_POSTS_QUERY } from "../utils/graphql";
 
-function DeleteButton({ postId, callback }) {
+function DeleteButton({ postId, commentId, callback }) {
   const [confirmOpen, setConfirmOpen] = useState(false);
 
-  const [deletePost] = useMutation(DELETE_POST_MUTATION, {
+  const mutation = commentId ? DELETE_COMMENT_MUTATION : DELETE_POST_MUTATION;
+
+  const [deletePostOrMutation] = useMutation(mutation, {
     update(proxy) {
       setConfirmOpen(false);
 
-      const data = proxy.readQuery({
-        query: FETCH_POSTS_QUERY,
-      });
+      if (!commentId) {
+        const data = proxy.readQuery({
+          query: FETCH_POSTS_QUERY,
+        });
 
-      proxy.writeQuery({
-        query: FETCH_POSTS_QUERY,
-        data: {
-          getPosts: data.getPosts.filter((p) => p.id !== postId),
-        },
-      });
+        proxy.writeQuery({
+          query: FETCH_POSTS_QUERY,
+          data: {
+            getPosts: data.getPosts.filter((p) => p.id !== postId),
+          },
+        });
+      }
 
       if (callback) callback();
     },
     variables: {
       postId,
+      commentId,
     },
   });
 
@@ -43,7 +48,7 @@ function DeleteButton({ postId, callback }) {
       <Confirm
         open={confirmOpen}
         onCancel={() => setConfirmOpen(false)}
-        onConfirm={deletePost}
+        onConfirm={deletePostOrMutation}
       />
     </>
   );
