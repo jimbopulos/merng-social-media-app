fe96ca7485c035429a83f24305f65027bf1d3875
James Galantino <james_galantino@aol.com>
2021-06-23 14:06:11 -0400

    Created delete comment mutation

--- client/src/components/DeleteButton.js
+++ client/src/components/DeleteButton.js
@@ -55,4 +55,19 @@ const DELETE_POST_MUTATION = gql`
   }
 `;
 
+const DELETE_COMMENT_MUTATION = gql`
+  mutation deleteComment($postId: ID!, $commentId: ID!) {
+    deleteComment(postId: $postId, commentId: $commentId) {
+      id
+      comments {
+        id
+        username
+        createdAt
+        body
+      }
+      commentCount
+    }
+  }
+`;
+
 export default DeleteButton;
