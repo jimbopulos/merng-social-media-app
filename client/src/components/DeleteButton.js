c0b6840f16b091a5ef17894357636b2d64fa16e3
James Galantino <james_galantino@aol.com>
2021-06-25 14:37:14 -0400

    Added info popup for delete button component

--- client/src/components/DeleteButton.js
+++ client/src/components/DeleteButton.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import gql from "graphql-tag";
 import { useMutation } from "@apollo/client";
 
-import { Button, Confirm, Icon } from "semantic-ui-react";
+import { Button, Confirm, Icon, Popup } from "semantic-ui-react";
 import { FETCH_POSTS_QUERY } from "../utils/graphql";
 
 function DeleteButton({ postId, commentId, callback }) {
@@ -37,14 +37,20 @@ function DeleteButton({ postId, commentId, callback }) {
 
   return (
     <>
-      <Button
-        as="div"
-        color="red"
-        floated="right"
-        onClick={() => setConfirmOpen(true)}
-      >
-        <Icon name="trash" style={{ margin: 0 }} />
-      </Button>
+      <Popup
+        content="Delete post"
+        inverted
+        trigger={
+          <Button
+            as="div"
+            color="red"
+            floated="right"
+            onClick={() => setConfirmOpen(true)}
+          >
+            <Icon name="trash" style={{ margin: 0 }} />
+          </Button>
+        }
+      />
       <Confirm
         open={confirmOpen}
         onCancel={() => setConfirmOpen(false)}
